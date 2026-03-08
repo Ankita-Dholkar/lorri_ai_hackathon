2639e34b903de479be4cd9496f1984452f78d880
Ankita-Dholkar <dholkarankita@gmail.com>
2026-03-08 14:07:40 +0530

    update

--- LogisticsHackathon/Backend/server.js
+++ LogisticsHackathon/Backend/server.js
@@ -15,7 +15,10 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:[
+    "http://localhost:5173",
+    "https://cargointellogistics.vercel.app"
+  ],
     credentials: true
 }))
 
