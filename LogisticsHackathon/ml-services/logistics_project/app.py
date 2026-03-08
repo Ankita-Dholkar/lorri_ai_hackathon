a8ea9c4f417070b3740745c47f5f6f7d76e42af1
Ankita-Dholkar <dholkarankita@gmail.com>
2026-03-08 16:07:01 +0530

    fix ML service deploy

--- LogisticsHackathon/ml-services/logistics_project/app.py
+++ LogisticsHackathon/ml-services/logistics_project/app.py
@@ -189,4 +189,5 @@ def store_manual_lr():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
