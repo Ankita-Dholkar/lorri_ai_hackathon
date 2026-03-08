581e79e90d4923c708c6bea352569c0f5275fefb
Ankita-Dholkar <dholkarankita@gmail.com>
2026-03-08 10:10:38 +0530

    Initial commit

---
+++ LogisticsHackathon/Backend/server.js
@@ -0,0 +1,34 @@
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
+import receiptRoutes from "./routes/receiptRoutes.js"
+import authRoutes from "./routes/authRoutes.js"
+import shipmentRoutes from "./routes/shipmentRoutes.js"
+import connectDb from "./config/connectDB.js"
+
+dotenv.config()
+
+const port = process.env.PORT
+const app = express();
+app.use(express.json())
+app.use(cookieParser())
+
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials: true
+}))
+
+
+app.use("/api/receipts", receiptRoutes)
+app.use("/api/auth", authRoutes)
+app.use("/api/shipments", shipmentRoutes)
+
+app.get("/",(req,res) => {
+    res.send("Hello from Server");
+})
+
+app.listen(port,() => {
+    console.log(`Server running on port ${port}`);
+    connectDb();
+})
